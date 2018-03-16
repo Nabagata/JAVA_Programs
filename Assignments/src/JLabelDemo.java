b8dc0c62c22288f6d58abef3ff02b6b54de33529
Nabagata <ngsaha234@gmail.com>
2018-03-16 17:20:52 +0530

    lol

---
+++ Assignments/src/JLabelDemo.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.swing.*;
+
+public class JLabelDemo extends JApplet {
+  public void init(){
+    try{
+      SwingUtilities.invokeAndWait(
+        new Runnable(){
+          public void run()
+        }
+      )
+    }
+  }
+}
