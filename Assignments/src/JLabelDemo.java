e7984d8fcb40a3f0050ed6ff3ef8585733f8a028
Nabagata <ngsaha234@gmail.com>
2018-03-17 18:08:16 +0530

    lol

--- Assignments/src/JLabelDemo.java
+++ Assignments/src/JLabelDemo.java
@@ -1,14 +1,25 @@
 import java.awt.*;
-import java.swing.*;
+import javax.swing.*;
 
 public class JLabelDemo extends JApplet {
   public void init(){
     try{
       SwingUtilities.invokeAndWait(
         new Runnable(){
-          public void run()..
+          public void run(){
+            makeGUI();
+          }
         }
-      )
+      );
     }
+    catch(Exception exc){
+      System.out.println("Can't create because of "+exc);
+    }
+  }
+  public void makeGUI(){
+
+    ImageIcon ii = new ImageIcon("download.png");
+    JLabel jl = new JLabel("Nabagata",ii,JLabel.CENTER);
+    add(jl);
   }
 }
